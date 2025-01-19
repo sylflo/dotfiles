541aab5bffbfcff95767eb43f3d803fc72a39958
sylflo <git@sylvain-chateau.com>
2025-01-19 21:39:40 +0100

    refactoring

--- fabric-nix/my_project/wallpaper/wallpaper/controller.py
+++ fabric-nix/my_project/wallpaper/wallpaper/controller.py
@@ -29,18 +29,18 @@ class Wallpaper:
         self.service.connect("go-to-page", self.go_to_page)
         self.service.connect("select-monitor", self.select_monitor)
         self.service.connect("select-image", self.select_image)
-        self._current_page = 1
-        self._total_pages = self._get_total_pages(
+        self.current_page = 1
+        self.total_pages = self._get_total_pages(
             SETTINGS.layout.img_per_row, SETTINGS.layout.row_per_page
         )
-        self._selected_monitors = []
-        self._selected_monitors_name = []
-        self._selected_image = None
+        self.selected_monitors = []
+        self.selected_monitors_name = []
+        self.selected_image = None
    
 
         if SETTINGS.main.pagination:
             wallpaper_rows = self._get_pagination_wallpaper_rows(
-                self._current_page - 1,
+                self.current_page - 1,
                 SETTINGS.layout.img_per_row,
                 SETTINGS.layout.row_per_page,
             )
@@ -50,7 +50,7 @@ class Wallpaper:
             )
         self._view = WallpaperView(
             service=self.service,
-            total_pages=self._total_pages,
+            total_pages=self.total_pages,
             monitors=self._get_monitors(),
             wallpaper_rows=wallpaper_rows,
         )
@@ -88,49 +88,49 @@ class Wallpaper:
         return rows
 
     def next_page(self, service):
-        if self._current_page < self._total_pages:
-            self._current_page = self._current_page + 1
+        if self.current_page < self.total_pages:
+            self.current_page = self.current_page + 1
             self._update_view(action='next')
 
     def previous_page(self, service):
-        if self._current_page > 1:
-            self._current_page = self._current_page - 1
+        if self.current_page > 1:
+            self.current_page = self.current_page - 1
             self._update_view(action='previous')
 
     def go_to_page(self, service, page_index: int):
-        if page_index > 0 and page_index <= self._total_pages:
-            action = 'next' if page_index > self._current_page else 'previous'
-            self._current_page = page_index
+        if page_index > 0 and page_index <= self.total_pages:
+            action = 'next' if page_index > self.current_page else 'previous'
+            self.current_page = page_index
             self._update_view(action=action)
 
     def select_monitor(self, service, widget, monitor_name):
-        if widget in self._selected_monitors:
+        if widget in self.selected_monitors:
             self._view.set_unselected_monitor(widget)
-            self._selected_monitors.remove(widget)
-            self._selected_monitors_name.remove(monitor_name)
+            self.selected_monitors.remove(widget)
+            self.selected_monitors_name.remove(monitor_name)
         else:
             self._view.set_selected_monitor(widget)
-            self._selected_monitors.append(widget)
-            self._selected_monitors_name.append(monitor_name)
+            self.selected_monitors.append(widget)
+            self.selected_monitors_name.append(monitor_name)
         self.update_monitor_image()
 
     def select_image(self, service, widget, image_name):
         self._view.set_selected_image(widget)
-        self._selected_image = image_name
+        self.selected_image = image_name
         self.update_monitor_image()
 
     def update_monitor_image(self):
-        if self._selected_image:
+        if self.selected_image:
             for widget, name in zip(
-                self._selected_monitors, self._selected_monitors_name
+                self.selected_monitors, self.selected_monitors_name
             ):
                 image_location = (
-                    f"{SETTINGS.main.wallpapers_folder}/{self._selected_image}"
+                    f"{SETTINGS.main.wallpapers_folder}/{self.selected_image}"
                 )
                 command = SETTINGS.swww.build_command(name, image_location)
                 # TODO add logger for command
                 subprocess.run(command)
-                self._view.update_monitor_image(widget, self._selected_image)
+                self._view.update_monitor_image(widget, self.selected_image)
                 shutil.copy(
                     image_location, Path(SETTINGS.config_file).parent / name
                 )
@@ -139,9 +139,9 @@ class Wallpaper:
         self._view.update_wallpaper_rows(
             service=self.service,
             action=action,
-            page_index=self._current_page,
+            page_index=self.current_page,
             wallpaper_rows=self._get_pagination_wallpaper_rows(
-                self._current_page - 1,
+                self.current_page - 1,
                 SETTINGS.layout.img_per_row,
                 SETTINGS.layout.row_per_page,
             ),
